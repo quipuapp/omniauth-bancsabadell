fb2863a794e7742246a52f2a2737eb426da5642f
Albert Bellonch Llargués <albert@itnig.net>
2014-10-11 13:20:35 +0200

    Temp fix for certificate verification error

--- test_app/config/initializers/omniauth.rb
+++ test_app/config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
+silence_warnings { OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development? }
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :bancsabadell,
     CUSTOM_CONFIG['banc_sabadell']['client_id'],
