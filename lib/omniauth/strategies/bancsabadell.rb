d912be2a378b456dfbee4bc5db6baa2381189b09
Albert Bellonch Llargués <albert@itnig.net>
2014-10-11 13:05:47 +0200

    Added uccessful request phase (response not working)

--- lib/omniauth/strategies/bancsabadell.rb
+++ lib/omniauth/strategies/bancsabadell.rb
@@ -4,6 +4,20 @@ module OmniAuth
   module Strategies
     class Bancsabadell < OmniAuth::Strategies::OAuth2
       API_BS_HOST = 'https://developers.bancsabadell.com'
+
+      option :client_options, {
+        site: 'https://developers.bancsabadell.com',
+        authorize_url: 'https://developers.bancsabadell.com/AuthServerBS/oauth/authorize',
+        token_url: 'https://developers.bancsabadell.com/AuthServerBS/oauth/token'
+      }
+
+      def authorize_params
+        super.merge({
+          response_type: :code,
+          client_id: options.client_id,
+          scope: "read write"
+        })
+      end
     end
   end
 end
--- test_app/config/initializers/omniauth.rb
+++ test_app/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :bancsabadell,
-    CUSTOM_CONFIG['banc_sabadell']['consumer_key'],
-    CUSTOM_CONFIG['banc_sabadell']['consumer_secret']
+    CUSTOM_CONFIG['banc_sabadell']['client_id'],
+    CUSTOM_CONFIG['banc_sabadell']['client_secret']
 end
